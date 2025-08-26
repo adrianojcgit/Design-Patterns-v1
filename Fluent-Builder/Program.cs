67033c927318179faf4bf62e68768fd1d19754ac
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-08-26 13:46:31 -0300

    Adicionar projeto Builder

---
+++ Fluent-Builder/Program.cs
@@ -0,0 +1,18 @@
+﻿using jcm_patterns_fluent_builder;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var emailBuilder = new EmailBuilder()
+                    .To("microsoft@gmail.com")
+                    .From("apple@gmail.com")
+                    .Subject("Patterns Fluent Builder")
+                    .Body("Microsoft build 2025\n\t25 a 27 de Setembro 2025" +
+                    "\n\tNovidades Windows, Azure, Office e mais");
+
+        var email = emailBuilder.CriaEmail();
+        Console.WriteLine(email.ToString());
+        Console.ReadLine();
+    }
+}
