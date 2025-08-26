92e4789855ddf3a6511e83ae9f8d075408bf2015
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-08-26 14:14:28 -0300

    Adicionar projeto Singleton tipo Safety Thread

---
+++ Singleton-Safety-Thread/README.md
@@ -0,0 +1,4 @@
+# jcm-patterns-singleton-thread-safety
+Exemplo do Design Patterns Singleton - Thread Safety com verificação dupla
+Créditos ao José Carlos Macoratti
+https://www.youtube.com/playlist?list=PLJ4k1IC8GhW1L7fOWe238fetknEfBmG1I
---
+++ Singleton-Safety-Thread/Singleton.cs
@@ -0,0 +1,34 @@
+namespace jcm_patterns_singleton_thread_safety
+{
+    public class Singleton
+    {
+        private static Singleton? instance = null;
+        private static readonly object lockObject = new object();
+        private int numeroDeInstancias = 0;
+        private Singleton()
+        {
+            Console.WriteLine("Instanciando dentro do construtor privado");
+            numeroDeInstancias++;
+            Console.WriteLine($"Numero de instâncias = {numeroDeInstancias}");
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (instance == null)
+                        {
+                            Console.WriteLine("Criando a primeira instância");
+                            instance = new Singleton();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
