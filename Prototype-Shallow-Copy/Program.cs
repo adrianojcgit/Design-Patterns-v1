299334c96ee78ddacd87d969b83f7b4517884154
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-08-26 13:54:00 -0300

    Adicionar o projeto Prototype tipo Shallow Copy

---
+++ Prototype-Shallow-Copy/Program.cs
@@ -0,0 +1,29 @@
+﻿using jcm_patterns_prototype_shallow_copy;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Cria objeto do tipo Soldado
+        Soldado soldado = new Soldado();
+        soldado.Nome = "Silva";
+        soldado.Arma = "Fuzil";
+
+        Soldado soldado_clone1 = (Soldado)soldado.Clone();
+        Soldado soldado_clone2 = (Soldado)soldado.Clone();
+
+        //exibe valores do objeto original
+        Console.WriteLine(">> Objeto Original");
+        Console.WriteLine($"   {soldado.Nome} - {soldado.Arma}");
+
+        //valores do objeto clone1
+        Console.WriteLine(">> Objeto Clone1");
+        Console.WriteLine($"   {soldado_clone1.Nome} - {soldado_clone1.Arma}");
+
+        //valores do objeto clone2
+        Console.WriteLine(">> Objeto Clone2");
+        Console.WriteLine($"   {soldado_clone2.Nome} - {soldado_clone2.Arma}");
+
+        Console.ReadLine();
+    }
+}
---
+++ Prototype-Shallow-Copy/README.md
@@ -0,0 +1,4 @@
+# jcm-patterns-prototype-shallow-copy
+Exemplo do Design Patterns Prototype Shallow Copy \n
+Créditos ao José Carlos Macoratti \n
+https://www.youtube.com/playlist?list=PLJ4k1IC8GhW1L7fOWe238fetknEfBmG1I
