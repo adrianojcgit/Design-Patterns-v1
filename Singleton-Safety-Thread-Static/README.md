11567a4120e7b0463783f9fe8cd02e99a5c30b58
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-08-26 14:18:56 -0300

    Adicionar projeto Singleton tipo Safety Thread com classe Static

---
+++ Singleton-Safety-Thread-Static/README.md
@@ -0,0 +1,4 @@
+# jcm-patterns-singleton-thread-safety-static
+Exemplo de Designer Patterns Singleton multithread com classe sealed e static
+Créditos ao José Carlos Macoratti https://www.youtube.com/playlist?list=PLJ4k1IC8GhW1L7fOWe238fetknEfBmG1I
+
---
+++ Singleton-Safety-Thread-Static/Singleton.cs
@@ -0,0 +1,23 @@
+namespace jcm_patterns_singleton_thread_safety_static
+{
+    public sealed class Singleton
+    {
+        private static int contador = 0;
+        private static readonly Singleton instance;
+        private Singleton() { }
+        static Singleton()
+        {
+            contador++;
+            Console.WriteLine($"Instancia: {contador}");
+            instance = new Singleton();
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
